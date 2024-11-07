ab0054328a765137e6ec44b23f65153d8c9361c8
CandeGod <Candebeatriz14@gmail.com>
2024-11-06 20:53:04 -0600

    ya se conecta conecta correctamente a la base de datos remota

--- Conexion/DatabaseConnection.cs
+++ Conexion/DatabaseConnection.cs
@@ -12,46 +12,13 @@ namespace RosticeriaCardel
         public MySqlConnection GetConnection()
         {
             
-            MySqlConnection conexion = new MySqlConnection("Server=localhost;Database=rosticeriacardel;Uid=root;Pwd=Cande213apo$;");
+            MySqlConnection conexion = new MySqlConnection("Server=b43pdopm39vxyqhdhvne-mysql.services.clever-cloud.com;Database=b43pdopm39vxyqhdhvne;Uid=ultjvjsxnryp45yf;Pwd=1l20eGFJNbDjJCWwK4Nd;");
 
             
             conexion.Open();
 
             return conexion;
         }
-
-        /*
-        public bool ExecRespaldoBD()
-{
-    bool result = true;
-    
-    // Ruta de OneDrive
-    string backupPath = @"C:\Users\Cristian\OneDrive\Pruebasql\";
-    
-    try
-    {
-        string spSQL = "sp_BackupFullDatabase";
-        using (SqlConnection connection = GetConnection())
-        {
-            using (SqlCommand command = new SqlCommand(spSQL, connection))
-            {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.Add(new SqlParameter("@BackupPath", SqlDbType.NVarChar, 255)
-                {
-                    Value = backupPath
-                });
-                command.ExecuteNonQuery();
-            }
-        }
-    }
-    catch (Exception ex)
-    {
-        MessageBox.Show("Error: " + ex.Message);
-        result = false;
-    }
-
-    return result;
-}*/
 
     }
 }
